ba75463006139f74d32575830bc552c59d59abed
Alex Pacheco <alex.pacheco.s@gmail.com>
2016-07-18 21:09:20 -0500

    Cascadas

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,6 +1,6 @@
 class Inventory < ActiveRecord::Base
   belongs_to :category
-  has_one :work
+  has_one :work, dependent: :destroy
   validates :size, numericality: {greater_than: 0}
 
   scope :filter_by_size, ->(query) { where(size: query)  }
